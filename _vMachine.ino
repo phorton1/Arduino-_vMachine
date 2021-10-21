50897576145a3f370bca8511afe8aeadc2dced59
phorton1 <available@upon.request>
2021-10-21 13:48:01 -0500

    documentation WIP: TFT_eSPI and FluidNC_UI and screen grabs

--- _vMachine.ino
+++ _vMachine.ino
@@ -30,61 +30,64 @@
     }
 
 
-    void user_realtime_command(uint8_t command, Print &client)
-    {
-        // ctrl keys
-        //
-        // ctrl-Q			CMD_LIVE_Z_PLUS_COARSE
-        // ctrl-W       	CMD_LIVE_Z_PLUS_FINE
-        // ctrl-E       	CMD_LIVE_Z_RESET
-        // ctrl-R       	CMD_LIVE_Z_MINUS_FINE
-        // ctrl-T       	CMD_LIVE_Z_MINUS_COARSE
-        //
-        // 0x01 - ctrl-A
-        // 0x02 - ctrl-B
-        // 0x03 - ctrl-C	concole exit
-        // 0x04 - ctrl-D	console clear
-        // 0x05 - ctrl-E	CMD_LIVE_Z_RESET
-        // 0x06 - ctrl-F	console experimental gcode
-        // 0x07 - ctrl-G	console gcode
-        // 0x08 - ctrl-H
-        // 0x09 - ctrl-I
-        // 0x0a - ctrl-J
-        // 0x0b - ctrl-K
-        // 0x0c - ctrl-L
-        // 0x0d - ctrl-M
-        // 0x0e - ctrl-N
-        // 0x0f - ctrl-O    CMD_UI_SCREEN_GRAB_PENDING
-        // 0x10 - ctrl-P    CMD_UI_SCREEN_GRAB
-        // 0x11 - ctrl-Q	CMD_LIVE_Z_PLUS_COARSE
-        // 0x12 - ctrl-R    CMD_LIVE_Z_MINUS_COARSE
-        // 0x13 - ctrl-S
-        // 0x14 - ctrl-T	CMD_LIVE_Z_MINUS_COARSE
-        // 0x15 - ctrl-U
-        // 0x16 - ctrl-V
-        // 0x17 - ctrl-W	CMD_LIVE_Z_PLUS_FINE
-        // 0x18 - ctrl-X	FluidNC reset (console upload if enabled)
-        // 0x19 - ctrl-Y
-        // 0x1a - ctrl-Z
-        // 0x1b - ESC
-        // 0x1c -
-        // 0x1d -
-        // 0x1e -
-        // 0x1f -
-
-        g_debug("user_realtime_command(0x%02x)",command);
-
-        switch (command)
+    #ifdef WITH_SCREEN_GRAB
+        void user_realtime_command(uint8_t command, Print &client)
         {
-			case CMD_UI_SCREEN_GRAB :
-				gApplication::doScreenGrab(false);
-				break;
-			case CMD_UI_SCREEN_GRAB_PENDING :
-				gApplication::doScreenGrab(true);
-				break;
+            // ctrl keys
+            //
+            // ctrl-Q			CMD_LIVE_Z_PLUS_COARSE
+            // ctrl-W       	CMD_LIVE_Z_PLUS_FINE
+            // ctrl-E       	CMD_LIVE_Z_RESET
+            // ctrl-R       	CMD_LIVE_Z_MINUS_FINE
+            // ctrl-T       	CMD_LIVE_Z_MINUS_COARSE
+            //
+            // 0x01 - ctrl-A
+            // 0x02 - ctrl-B
+            // 0x03 - ctrl-C	concole exit
+            // 0x04 - ctrl-D	console clear
+            // 0x05 - ctrl-E	CMD_LIVE_Z_RESET
+            // 0x06 - ctrl-F	console experimental gcode
+            // 0x07 - ctrl-G	console gcode
+            // 0x08 - ctrl-H
+            // 0x09 - ctrl-I
+            // 0x0a - ctrl-J
+            // 0x0b - ctrl-K
+            // 0x0c - ctrl-L
+            // 0x0d - ctrl-M
+            // 0x0e - ctrl-N
+            // 0x0f - ctrl-O    CMD_UI_SCREEN_GRAB_PENDING
+            // 0x10 - ctrl-P    CMD_UI_SCREEN_GRAB
+            // 0x11 - ctrl-Q	CMD_LIVE_Z_PLUS_COARSE
+            // 0x12 - ctrl-R    CMD_LIVE_Z_MINUS_COARSE
+            // 0x13 - ctrl-S
+            // 0x14 - ctrl-T	CMD_LIVE_Z_MINUS_COARSE
+            // 0x15 - ctrl-U
+            // 0x16 - ctrl-V
+            // 0x17 - ctrl-W	CMD_LIVE_Z_PLUS_FINE
+            // 0x18 - ctrl-X	FluidNC reset (console upload if enabled)
+            // 0x19 - ctrl-Y
+            // 0x1a - ctrl-Z
+            // 0x1b - ESC
+            // 0x1c -
+            // 0x1d -
+            // 0x1e -
+            // 0x1f -
+
+            g_debug("user_realtime_command(0x%02x)",command);
+
+            switch (command)
+            {
+                case CMD_UI_SCREEN_GRAB :
+                    gApplication::doScreenGrab(false);
+                    break;
+                case CMD_UI_SCREEN_GRAB_PENDING :
+                    gApplication::doScreenGrab(true);
+                    break;
+            }
         }
-    }
-#endif
+    #endif  // WITH_SCREEN_GRAB
+#endif  // WITH_UI
+
 
 
 void setup()
